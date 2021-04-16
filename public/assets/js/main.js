0b8bfa2b8cf747e7f045b39a4e803c1a226367ff
Nadia Murias <nadiamurias@gmail.com>
2021-04-17 01:10:56 +0200

    Add main structure

---
+++ public/assets/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(">> Ready :)");
+
+let animalCards = new Array();
+animalCards[0] = "/images/oveja.png";
+animalCards[1] = "/images/oveja.png";
+console.log(animalCards);
+
+const vaca = document.querySelector(".card");
+
+const animales = ["🐖", "🐑"];
+
+console.log(animales);
+
+//# sourceMappingURL=main.js.map
